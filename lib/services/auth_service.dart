5fd804a19c00504f9abaad81b4d7b688f3077f42
imak <imakkamikill@gmail.com>
2024-07-06 13:37:02 -0300

    edited screens login

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future<void> signIn(String email, String password) async {
-    await _auth.signInWithEmailAndPassword(email: email, password: password);
+  Future<UserCredential> signIn(String email, String password) async {
+    return await _auth.signInWithEmailAndPassword(email: email, password: password);
   }
 }
